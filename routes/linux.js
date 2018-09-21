fceaa9d41566c0f93e8afcb4eb733e7d97961c1f
나 <gds02131@naver.com>
2018-09-21 14:40:10 +0900

    푸시 서비스 테스트
    Signed-off-by: 나 <gds02131@naver.com>

--- routes/linux.js
+++ routes/linux.js
@@ -2,38 +2,54 @@ module.exports = function (current_video_call_state) {
     var express = require('express');
     var router = express.Router();
     var app = express();
-    const server = require('http').createServer(app);
-    const io = require('socket.io')(server);
+    var FCM = require('fcm-node');
+    var fcm = new FCM("AAAAnXVPz8A:APA91bG_JoiQOkaYCcZxsX8JOfIQ5vgrhdqnNFImpqZvoAegqObmz-kFrnn-e25xjBtp-KZkhOfqXvM83wU1BBazKZ8O2KYJF8LUR0zaJlzicRs5VzyWOz0MkserOvCXXGpRJtYKfrbz");
+    var client_token = 'eB6NEGJMpJw:APA91bHKcYppGbtT9teUKwe-lufqcG5o-GfF-7WsIdS1bcRPmgyp3b3b3JYfcieyHiVRpGPddBjRMuouw7QfdFbv2fsFjJOFfvtQkbeBQGJkMiWJkx6746SQWQ_zWHJmVsGOtPdcaCAJ';
+    /** 발송할 Push 메시지 내용 */
+    var push_data = {
+        // 수신대상
+        to: client_token,
+        // App이 실행중이지 않을 때 상태바 알림으로 등록할 내용
+        notification: {
+            title: "Hello Node",
+            body: "Node로 발송하는 Push 메시지 입니다.",
+            sound: "default",
+            click_action: "FCM_PLUGIN_ACTIVITY",
+            icon: "fcm_push_icon"
+        },
+        // 메시지 중요도
+        priority: "high",
+        // App 패키지 이름
+        restricted_package_name: "com.example.word.raspberrytest",
+        // App에게 전달할 데이터
+        data: {
+            num1: 2000,
+            num2: 3000
+        }
+    };
 
 //linux/video_call_request - Android App에서 통화 거절 0 통화 승인 1로 response
     router.get('/video_call_request', function(req, res, next) {
         //영상통화가 있음을 Android App에 알림 - 푸시 -- 소켓써야 될듯 socket.io - 양방향 통신 구현
-        io.on('connection', (socket) => {
-            console.log('Android App과 연결되었습니다.');
-            io.to(socket.id).emit('video call request event', {msg:"video_call_request"}); //연결된 특정 클라이언트에만 전송
 
-            socket.on('disconnect', function() {
-                console.log('Android App과 연결이 끊어졌습니다.');
-            });
-
-            socket.on('receive message', function(result) {
-                //결과 받아서
-
-                if(result == 0) {
-                    res.json({ msg:"통화가 거절되었습니다.", return:0});
-                } else {
-                    res.json({ msg:"통화가 승인되었습니다.", return:1});
-                }
-
-            });
-        });
         res.json({ msg:"Android App이 socket listen 하지 않음", return:-1});
     });
 
 //linux/voice_mail_save
-    router.post('/voice_mail_save', function(req, res, next) {
+    router.get('/voice_mail_save', function(req, res, next) {
         //받은 음성파일과 함께 현재시각 DB에 저장
         //후에 웹브라우저와 안드로이드에게 푸시메시지 발송
+        fcm.send(push_data, function(err, response) {
+            if (err) {
+                console.error('Push메시지 발송에 실패했습니다.');
+                console.error(err);
+                return;
+            }
+
+            console.log('Push메시지가 발송되었습니다.');
+            console.log(response);
+        });
+        res.send("여기 들어와지긴 하는거?");
     });
 
 //linux/video_call_start
--- routes/video_call_streaming.js
+++ routes/video_call_streaming.js
@@ -1,8 +1,66 @@
 var express = require('express');
 var router = express.Router();
 
+var url = 'rtsp://192.168.0.22:8554/test'
+//var stream = new rtsp.FFMpeg({input: uri});
+/*
+io.on('connection', function(socket) {
+    var pipeStream = function(data) {
+        socket.emit('data', data.toString('base64'));
+    };
+    stream.on('data', pipeStream);
+    socket.on('disconnect', function() {
+        stream.removeListener('data', pipeStream);
+    });
+});
+*/
 router.get('/', function(req, res, next) {
-    res.render('video_call_streaming');
+  /*  var cmd = 'gst-launch-1.0';
+   // var args = ['-v', 'tcpclientsrc', 'host=192.168.0.22', 'port=5000', '!',
+       // 'gdpdepay', '!', 'rtph264depay', '!', 'avdec_h264', '!', 'videoconvert', '!', 'autovideosink',
+       // 'sync=false'];
+    var args = ['autovideosrc', 'horizontal-speed=1', 'is-live=true',
+        '!', 'videoconvert',
+        '!', 'vp8enc', 'cpu-used=5', 'deadline=1', 'keyframe-max-dist=10',
+        '!', 'queue', 'leaky=1',
+        '!', 'm.', 'webmmux', 'name=m', 'streamable=true',
+        '!', 'queue', 'leaky=1',
+        '!', 'tcpserversink', 'host=192.168.0.22', 'port=5000', 'sync-method=2'];
+    var child = require('child_process');
+    var gstreamer = child.spawn(cmd, args, {stdio: 'ignore'});
+
+    gstreamer.on('exit', function (code) {
+        if (code != null) {
+            console.log('GStreamer error, exit code ' + code);
+        }
+        process.exit();
+    });
+
+    // res.render('video_call_str;eaming')
+    var date = new Date();
+
+    res.writeHead(200, {
+        'Date': date.toUTCString(),
+        'Connection': 'close',
+        'Cache-Control': 'private',
+        'Content-Type': 'video/webm',
+        'Server': 'CustomStreamer/0.0.1',
+    });
+
+    var net = require('net');
+    var socket = net.connect(5000, function () {
+        socket.on('close', function (had_error) {
+            res.end();
+        });
+        socket.on('data', function (data) {
+            res.write(data);
+        });
+    });
+    socket.on('error', function (error) {
+        console.log(error);
+    });
+    */
+
 });
 
 module.exports = router;
