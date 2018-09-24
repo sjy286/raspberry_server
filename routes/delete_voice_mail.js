95d9066d6bdc2a76764a8818092f9084721f0691
나 <gds02131@naver.com>
2018-09-24 23:55:14 +0900

    음성메시지 테이블 삭제 구현 완료
    Signed-off-by: 나 <gds02131@naver.com>

--- routes/delete_voice_mail.js
+++ routes/delete_voice_mail.js
@@ -4,21 +4,58 @@ var router = express.Router();
 var DBManager = require('../database/DBManager.js');
 var conn = DBManager.getConnection();
 
-var delete_voice_mail_sql = 'select log.log_no, current_datetime, memo, file from log, writes where log.log_no = writes.log_no;';
+var search_voice_mail_file_name = 'select file from writes where log_no = ?';
+var delete_voice_mail_sql_1 = 'delete from log where log_no = ?;';
+var delete_voice_mail_sql_2 = 'delete from writes where log_no = ?;';
+//delete from log where log_no = ?
+//delete from writes where log_no = ?
+var fs = require('fs');
 
-router.post('/', function(req, res, next) {
-    console.log(req.body.voice_mail_log);
-    conn.query(delete_voice_mail_sql, function(err, rows, fields) {
-        if(err) {
+function delete_voice_mail(param) {
+    conn.query(search_voice_mail_file_name, param, function (err, rows, fields) {
+        if (err) {
             console.log(err);
-        }  else {
+        } else {
             console.log(rows);
-            res.render('voice_mail_log', {
-                row : rows,
-                length : rows.length
+            var file_name = rows[0].file.split('/');
+            //파일도 삭제해야 됨
+            fs.unlink('./upload/voice_mail/' + file_name[2], err => {
+                if (err) 
+                    return console.log(err);
+                }
+                console.log('file is deleted');
             });
         }
     });
-});
+    conn.query(delete_voice_mail_sql_1, param, function (err, rows, fields) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('log테이블에서 삭제 완료');
+        }
+    });
+    conn.query(delete_voice_mail_sql_2, param, function (err, rows, fields) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('writes테이블에서 삭제 완료');
+        }
+    });
+};
 
+router.post('/', function (req, res, next) {
+    console.log(req.body.voice_mail_log);
+    //파일 하나 삭제 또는 그냥 삭제눌렀을 때 예외처리 해야 됨
+    console.log(Array.isArray(req.body.voice_mail_log));
+    if (!Array.isArray(req.body.voice_mail_log)) {
+        var param = req.body.voice_mail_log;
+        delete_voice_mail(param);
+    } else {
+        for (var i = 0; i < req.body.voice_mail_log.length; i++) {
+            var param = req.body.voice_mail_log[i];
+            delete_voice_mail(param);
+        }
+    }
+    res.redirect('/voice_mail_log');  //log페이지 갱신
+});
 module.exports = router;
